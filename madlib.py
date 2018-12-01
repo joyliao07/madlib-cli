1e827a4446aaffb8ea6290cc77498be0b5f8095e
Joyce Liao <joyliao07@gmail.com>
2018-11-30 19:57:29 -0800

    refactored functions

--- madlib.py
+++ madlib.py
@@ -1,6 +1,6 @@
 """Madlib program that replace user's inputs in the template text string."""
-
-
+from textwrap import dedent
+WIDTH = 71
 
 def trypytest():
     """To test pytest setup."""
@@ -27,36 +27,50 @@ def check_string(new):
         if what == '}':
             rec = False
             break
-    question_1(ques, new)
-    return (ques, new)    ############
+    return(ques, new)
 
 
 def question_1(ques, new):
     """Prompt the user to enter their word and pass the word to the next function."""
     print('Please enter a(n) ' + ques + ' of your choice.')
     user_input = input()
-    update_newlist(ques, new, user_input)
-    return (ques, new, user_input) ###########
+    return(ques, new, user_input)
 
 
 def update_newlist(ques, new, user_input):
     """Update the 'new' string with user's input and decide the next step."""
     new = new.replace(ques, user_input, 1)
-    if '{' in new:
-        check_string(new)
-        return(new)
-    else:
-        with open('outfile.txt', 'w') as wf:
-            wf.write(new)
-        print('Following is your Madlib result:')
-        print(new)
-        print('I hope you had fun.')
-        print('Thank you for playing.')
-        exit()
-        return(new)
+    return(new)
+
 
 if __name__ == '__main__':
     with open('infile.txt') as f:
         new = f.read()
+
     welcome(new)
-    check_string(new)
+    while '{' in new:
+        ques, new = check_string(new)
+        ques, new, user_input = question_1(ques, new)
+        new = update_newlist(ques, new, user_input)
+
+with open('outfile.txt', 'w') as wf:
+    wf.write(new)
+
+print(dedent(f'''
+{('Following is your Madlib result:')}
+{('** ' * (WIDTH//3) )}
+{(new)}
+{('** ' * (WIDTH//3) )}
+{(' ' * WIDTH)}
+{(' ' * WIDTH)}
+{('I hope you had fun. Thank you for playing.')}
+        '''))
+exit()
+
+
+
+
+
+
+
+
